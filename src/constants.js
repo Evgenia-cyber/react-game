28f4ef381aa2c69acbd80e9767c779592b9ea55d
Evgenia Kurineva <kurineva85@mail.ru>
2021-02-20 11:29:45 +0300

    feat: make the animation - snake move to the right

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,24 @@
 const FIELD_WIDTH = 400;
 const FIELD_LENGTH = 400;
 const CELL = 20;
-const APPLE = { left: 300, top: 100 };
-const SNAKE_HEAD = { left: 190, top: 150 };
+const SPEED = 200;
+// const CELL = 1;///////змея идет плавно
+// const SPEED = 20;///////змея идет плавно
+const APPLE = { left: 290, top: 100 };
+const SNAKE_HEAD = { left: 190, top: 200 };
 const SNAKE_BODY = [
-  { left: 170, top: 150 },
-  { left: 150, top: 150 },
+  { left: 170, top: 200 },
+  { left: 150, top: 200 },
 ];
+const CURRENT_DIRECTION = { left: CELL, top: 0 };
 
-export { FIELD_WIDTH, FIELD_LENGTH, CELL, APPLE, SNAKE_HEAD, SNAKE_BODY };
+export {
+  FIELD_WIDTH,
+  FIELD_LENGTH,
+  CELL,
+  APPLE,
+  SNAKE_HEAD,
+  SNAKE_BODY,
+  SPEED,
+  CURRENT_DIRECTION
+};
