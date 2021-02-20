2b69926057c353e058a937eece1a16a990e3e812
Evgenia Kurineva <kurineva85@mail.ru>
2021-02-20 10:32:44 +0300

    feat: make up the layout ot the game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import {
+  FIELD_LENGTH,
+  FIELD_WIDTH,
+  SNAKE_HEAD,
+  APPLE,
+  SNAKE_BODY,
+} from './constants';
+import './App.css';
+import appleImg from './assets/img/apple.svg';
+import awardImg from './assets/img/award.svg';
+import volumeImg from './assets/img/sound.svg';
+import closeImg from './assets/img/close.svg';
+
+const App = () => {
+  const style = {
+    field: {
+      width: `${FIELD_WIDTH}px`,
+      height: `${FIELD_LENGTH}px`,
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="wrapper">
+        <h1>SNAKE GAME</h1>
+        <div className="container">
+          <div className="toolbar">
+            <div className="left">
+              <div className="toolbar_item">
+                <img className="score_img" src={appleImg} alt="score" />
+                <span className="score">1</span>
+              </div>
+              <div className="toolbar_item">
+                <img
+                  className="bestscore_img"
+                  src={awardImg}
+                  alt="best score"
+                />
+                <span className="score">100</span>
+              </div>
+            </div>
+            <div className="right">
+              <div className="volume toolbar_item">
+                <img src={volumeImg} alt="volume" />
+                {/* <div className="volume-control">
+                  <span className="volume-control_line"></span>
+                  <span className="volume-control_slider"></span>
+                </div> */}
+              </div>
+              <div className="close toolbar_item">
+                <img src={closeImg} alt="close" />
+              </div>
+            </div>
+          </div>
+
+          <div style={style.field} className="game_field">
+            <div className="snake">
+
+              <div
+                className="snake_head game_item"
+                style={{
+                  left: `${SNAKE_HEAD.left}px`,
+                  top: `${SNAKE_HEAD.top}px`,
+                }}
+              ></div>
+
+              {SNAKE_BODY.map((item, index) => (
+                <div
+                  key={index}
+                  className="snake_body game_item"
+                  style={{
+                    left: `${item.left}px`,
+                    top: `${item.top}px`,
+                  }}
+                ></div>
+              ))}
+            </div>
+            
+            <div
+              className="apple game_item"
+              style={{ left: `${APPLE.left}px`, top: `${APPLE.top}px` }}
+            >
+              <img src={appleImg} alt="apple" />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* /////// */}
+      {/* <div className="modal active">
+        <button className="play_btn"><span>PLAY</span></button>
+      </div> */}
+      {/* /////// */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const FIELD_WIDTH = 400;
+const FIELD_LENGTH = 400;
+const CELL = 20;
+const APPLE = { left: 300, top: 100 };
+const SNAKE_HEAD = { left: 190, top: 150 };
+const SNAKE_BODY = [
+  { left: 170, top: 150 },
+  { left: 150, top: 150 },
+];
+
+export { FIELD_WIDTH, FIELD_LENGTH, CELL, APPLE, SNAKE_HEAD, SNAKE_BODY };
